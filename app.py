991ced1cc2e42381cb07346690ab1c18ec6df126
Pratya Chugh <pratyachugh@gmail.com>
2020-09-01 05:32:14 +0530

    and or query and UI update

--- app.py
+++ app.py
@@ -2,26 +2,46 @@ from flask import Flask,render_template,request,redirect,url_for
 import json,os
 app = Flask(__name__)
 
-with open("extractedData.json","r") as f:
+with open("./Data/extractedData.json","r") as f:
     mydict=json.load(f)
 
-results=[]
+results=[]        # final list that will contain names of requires files with keywords
+
+def result_creation(mylist):
+    numbr_of_words=len(mylist)
+
+    if numbr_of_words==1:
+        for file_name in mydict.keys():
+            words_dict=mydict[file_name]
+            if mylist[0] in words_dict.keys():
+                    results.append(file_name)
+           
+          
+    else:
+        if(mylist[1]=="OR"):
+             for file_name in mydict.keys():
+                words_dict=mydict[file_name]
+                if mylist[0] in words_dict.keys() or mylist[2] in words_dict.keys():
+                    results.append(file_name)
+
+
+        else:
+                 for file_name in mydict.keys():
+                    words_dict=mydict[file_name]
+                    if mylist[0] in words_dict.keys() or mylist[2] in words_dict.keys():
+                            results.append(file_name)
+        
+
 
 @app.route('/',methods=["POST","GET"])
 def hello_world():
     if request.method =="POST":
         pattern=request.form["to_search"]
-        #mylist=pattern.split()
-        #while "OR" in mylist:
-        #   mylist.remove("OR")
-        #while "AND" in mylist:
-        #   mylist.remove("AND")
+        mylist=pattern.split()
+
         results.clear()
-        for file_name in mydict.keys():
-            words_dict=mydict[file_name]
-            if pattern in words_dict.keys():
-                results.append(file_name)
-        print(results)
+
+        result_creation(mylist)
 
         return redirect("/searchresults")
     else:
@@ -29,7 +49,18 @@ def hello_world():
 
 @app.route('/searchresults',methods=["POST","GET"])
 def result():
-    return render_template("page2.html",file_names=results,curr_path=os.getcwd())
+    if request.method=="GET":
+         return render_template("page2.html",file_names=results)
+    else:
+        pattern=request.form["to_search"]
+        mylist=pattern.split()
+
+        results.clear()
+
+        result_creation(mylist)
+
+        return redirect("/searchresults")
+          
 
 
 if __name__=="__main__":
